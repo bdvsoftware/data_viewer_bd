54f465c18cfcb2646f4b00735e433b24e320eacf
bdvsoftware <bdvsoftware@gmail.com>
2025-04-07 17:03:39 +0200

    renames

--- data_viewer_init.sql
+++ data_viewer_init.sql
@@ -1,8 +1,8 @@
 CREATE TABLE grand_prix (
     gp_id SERIAL PRIMARY KEY,
-    nombre VARCHAR(255) NOT NULL,
-    fecha DATE NOT NULL,
-    circuito VARCHAR(255) NOT NULL
+    name VARCHAR(255) NOT NULL,
+    date DATE NOT NULL,
+    circuit VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE Session_Type (
@@ -21,11 +21,11 @@ CREATE TABLE Session (
 
 CREATE TABLE Video (
     video_id SERIAL PRIMARY KEY,
-    Session_id INT UNIQUE NOT NULL,
+    session_id INT UNIQUE NOT NULL,
     name TEXT,
     url TEXT NOT NULL,
     duration FLOAT NOT NULL,
-    FOREIGN KEY (Session_id) REFERENCES Session(Session_id)
+    FOREIGN KEY (session_id) REFERENCES Session(session_id)
 );
 
 CREATE TABLE Frame (
@@ -49,7 +49,7 @@ CREATE TABLE Driver (
 );
 
 CREATE TABLE Onboard_Frame (
-    onboard_id SERIAL PRIMARY KEY,
+    onboard_frame_id SERIAL PRIMARY KEY,
     frame_id INT NOT NULL,
     driver_id INT,
     timestamp TIMESTAMP NOT NULL,
@@ -58,7 +58,7 @@ CREATE TABLE Onboard_Frame (
 );
 
 CREATE TABLE Drivereye_Frame (
-    drivereye_id SERIAL PRIMARY KEY,
+    drivereye_frame_id SERIAL PRIMARY KEY,
     frame_id INT NOT NULL,
     driver_id INT,
     timestamp TIMESTAMP NOT NULL,
@@ -67,7 +67,7 @@ CREATE TABLE Drivereye_Frame (
 );
 
 CREATE TABLE Pitboost_Frame (
-    pitboost_id SERIAL PRIMARY KEY,
+    pitboost_frame_id SERIAL PRIMARY KEY,
     frame_id INT NOT NULL,
     status INT,
     timestamp TIMESTAMP NOT NULL,
@@ -75,16 +75,16 @@ CREATE TABLE Pitboost_Frame (
 );
 
 CREATE TABLE Pitboost_frame_Driver (
-    pitboost_id INT NOT NULL,
+    pitboost_frame_id INT NOT NULL,
     driver_id INT NOT NULL,
-    PRIMARY KEY (pitboost_id, driver_id),
-    FOREIGN KEY (pitboost_id) REFERENCES Pitboost_Frame(pitboost_id) ON DELETE CASCADE,
+    PRIMARY KEY (pitboost_frame_id, driver_id),
+    FOREIGN KEY (pitboost_frame_id) REFERENCES Pitboost_Frame(pitboost_frame_id) ON DELETE CASCADE,
     FOREIGN KEY (driver_id) REFERENCES Driver(driver_id) ON DELETE CASCADE
 );
 
 
 CREATE TABLE Wideshot_Frame (
-    wideshot_id SERIAL PRIMARY KEY,
+    wideshot_frame_id SERIAL PRIMARY KEY,
     frame_id INT NOT NULL,
     timestamp TIMESTAMP NOT NULL,
     video_url TEXT,
@@ -92,11 +92,11 @@ CREATE TABLE Wideshot_Frame (
     FOREIGN KEY (frame_id) REFERENCES Frame(frame_id)
 );
 
-CREATE TABLE Driver_Wideshot_Frame (
+CREATE TABLE Wideshot_Frame_Driver (
     driver_id INT NOT NULL,
-    wideshot_id INT NOT NULL,
-    PRIMARY KEY (driver_id, wideshot_id),
+    wideshot_frame_id INT NOT NULL,
+    PRIMARY KEY (driver_id, wideshot_frame_id),
     FOREIGN KEY (driver_id) REFERENCES Driver(driver_id),
-    FOREIGN KEY (wideshot_id) REFERENCES Wideshot_Frame(wideshot_id)
+    FOREIGN KEY (wideshot_frame_id) REFERENCES Wideshot_Frame(wideshot_frame_id)
 );
 
